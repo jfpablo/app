573fb9b1b25378ba79270cec38b83ebf00dd339e
jfpablo <103375465+jfpablo@users.noreply.github.com>
2022-07-12 01:54:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+setTimeout(function(){
+    load();
+},2000);
+function load()
+{
 cont.innerHTML = "chargement en cours";
-body.style.background = "#086cef";
+  body.style.background = "#086cef";
+}
+setTimeout(function(){
+    loaded();
+},6000);
+function loaded()
+{
+cont.innerHTML = "AMT CARE CENTER";
+  body.style.background = "white";
+}
