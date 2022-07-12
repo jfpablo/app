9d44532758534b2b031bec62d2285147ed7b9a17
jfpablo <103375465+jfpablo@users.noreply.github.com>
2022-07-12 23:54:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+ document.onreadystatechange = function() 
+  {
+    if (document.readyState != "complete") 
+    {
+      document.querySelector("body").style.visibility = "hidden";
+      document.querySelector("#chargement").style.visibility = "visible";
+    } 
+    else 
+    {
+      document.querySelector("#chargement").style.display = "none";
+      document.querySelector("body").style.visibility = "visible";
+    }
+  };
 load();
 function load()
 {
