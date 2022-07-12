dd5c3da1de45fddda0fb6c6e19f50949246060bd
jfpablo <103375465+jfpablo@users.noreply.github.com>
2022-07-12 23:58:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,17 +11,3 @@
       document.querySelector("body").style.visibility = "visible";
     }
   };
-load();
-function load()
-{
-cont.innerHTML = "chargement en cours";
-  body.style.background = "#086cef";
-    setTimeout(function(){
-    loaded();
-},5000);
-}
-function loaded()
-{
-cont.innerHTML = "AMT CARE CENTER";
-  body.style.background = "white";
-}
