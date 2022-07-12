a8b5b8f5bbd96ecfae17a540c3dd8841fede9fab
jfpablo <103375465+jfpablo@users.noreply.github.com>
2022-07-12 02:09:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,4 @@ function loaded()
 {
 cont.innerHTML = "AMT CARE CENTER";
   body.style.background = "white";
-  document.body.onload = addElement;
-
-function addElement () {
-  // crée un nouvel élément div
-  var newDiv = document.createElement("div");
-  // et lui donne un peu de contenu
-  var newContent = document.createTextNode('Hi there and greetings!');
-  // ajoute le nœud texte au nouveau div créé
-  newDiv.appendChild(newContent);
-
-  // ajoute le nouvel élément créé et son contenu dans le DOM
-  var currentDiv = document.getElementById('cont');
-  document.body.insertBefore(newDiv, currentDiv);
-}
 }
