4e1c50d50834d55ced9df90c1fea935c1969d862
jfpablo <103375465+jfpablo@users.noreply.github.com>
2022-11-21 13:42:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
- document.onreadystatechange = function() 
-  {
-    if (document.readyState != "complete") 
-    {
-      document.querySelector("body").style.visibility = "hidden";
-      document.querySelector("#chargement").style.visibility = "visible";
-    } 
-    else 
-    {
-      document.querySelector("#chargement").style.display = "none";
-      document.querySelector("body").style.visibility = "visible";
-     alert('cette app est en cours de mise à jour');
-    }
-  };
+
